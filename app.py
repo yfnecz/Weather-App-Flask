8d05503a93f4757240901abdb27f2641699618fa
Natalia Shishina <nshishina@odin.com>
2024-09-03 10:45:30 -0700

    stage5

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-import sys
-import requests
-from flask import Flask, render_template, request
+import sys, requests, json, datetime
+from flask import Flask, render_template, request, redirect, flash
 from flask_restful import reqparse
-import json
-from urllib.parse import quote
+from geopy.geocoders import Nominatim
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
 
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+
+db.create_all()
 
 parser = reqparse.RequestParser()
 parser.add_argument(
@@ -20,43 +31,60 @@ parser.add_argument(
 weather_api_key = ''
 weather_codes = []
 
-with open("../api.key", "r") as api_file:
+with open("/Users/Natali/Documents/work/PycharmProjects/Weather App/Weather App/api.key", "r") as api_file:
     weather_api_key = api_file.readline()
 
-with open("weather_code.json", "r") as code_file:
-    weather_codes = json.loads(code_file.read())
+geolocator = Nominatim(user_agent="abcd")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+    cities = []
     if request.method == 'POST':
-        city = request.form.get('city_name')
-        api_url = f'https://api.tomorrow.io/v4/weather/realtime?location={quote(city)}&apikey={weather_api_key}'
-        response = requests.get(api_url)
+        new_name = request.form.get('city_name')
+        if City.query.filter_by(name=new_name).first() is None:
+            location = geolocator.geocode(new_name)
+            if location is not None:
+                new_city = City(name=new_name)
+                db.session.add(new_city)
+                db.session.commit()
+            else:
+                flash("The city doesn't exist!")
+        else:
+            flash("The city has already been added to the list!")
+    api_url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}'
+    for city in City.query.all():
+        location = geolocator.geocode(city.name)
+        response = requests.get(api_url.format(location.latitude, location.longitude, weather_api_key))
         if response.status_code == requests.codes.ok:
             weather = json.loads(response.text)
-            temp = int(weather['data']['values']['temperature'])
-            state = weather_codes[str(weather['data']['values']['weatherCode'])]
-            weather = {'city': city, 'temp': temp, 'state': state}
-            return render_template('index.html', weather=weather)
+            temp = int(weather['main']['temp'])
+            state = weather['weather'][0]['main']
+
+            utc_now = datetime.datetime.fromtimestamp(weather['dt']) # time in utc seconds
+            timezone = datetime.timezone(datetime.timedelta(seconds=weather['timezone']))
+            hour = utc_now.astimezone(timezone).hour
+
+            time_of_day = 'day' if 19 > hour > 11 else 'evening-morning'
+            time_of_day = 'night' if hour < 5 or hour > 22 else time_of_day
+            cities.append({'city': city.name, 'temp': temp, 'state': state, 'time_of_day': time_of_day, 'city_id': city.id})
         else:
             return f"Error: {response.status_code}, {response.text}"
+    return render_template('index.html', cities=cities)
 
 
+@app.route('/delete/<int:city_id>', methods=['POST'])
+def delete_city(city_id):
+    city = City.query.filter_by(id=city_id).first()
+    if city:
+        db.session.delete(city)
+        db.session.commit()
+    return redirect("/")
 
-@app.route('/profile')
-def profile():
-    return 'This is profile page'
-
-
-@app.route('/login')
-def log_in():
-    return 'This is login page'
 
 
 if __name__ == '__main__':
+    app.secret_key = 'HelloWorld'
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
