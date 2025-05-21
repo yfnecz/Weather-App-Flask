883ea1899d5b4d89f819f4857e826ef7028e1f09
Natalia Shishina <nshishina@odin.com>
2025-05-21 15:05:07 -0700

    add error geolocate handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys, requests, json, datetime
 from flask import Flask, render_template, request, redirect, flash, jsonify
 from flask_restful import reqparse
+from geopy.exc import GeocoderServiceError
 from geopy.geocoders import Nominatim
 from flask_sqlalchemy import SQLAlchemy
 
@@ -75,49 +76,55 @@ def index():
     if request.method == 'POST':
         new_name = request.form.get('city_name')
         if City.query.filter_by(name=new_name).first() is None:
-            location = geolocator.geocode(new_name)
-            if location is not None:
-                new_city = City(name=new_name)
-                db.session.add(new_city)
-                db.session.commit()
-            else:
-                flash("The city doesn't exist!")
+            try:
+                location = geolocator.geocode(new_name)
+                if location is not None:
+                    new_city = City(name=new_name)
+                    db.session.add(new_city)
+                    db.session.commit()
+                else:
+                    flash("The city doesn't exist!")
+            except GeocoderServiceError as e:
+                flash(f"Geocoding service error: {e}")
         else:
             flash("The city has already been added to the list!")
     api_url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}'
     for city in City.query.all():
         if city.timestamp is None or city.timestamp + datetime.timedelta(hours=1) < datetime.datetime.now():
-            if city.name != new_name:
-                location = geolocator.geocode(city.name)
-            response = requests.get(api_url.format(location.latitude, location.longitude, weather_api_key))
-            if response.status_code == requests.codes.ok:
-                weather = json.loads(response.text)
-                temp = int(weather['main']['temp'])
-                state = weather['weather'][0]['main']
+            try:
+                if city.name != new_name:
+                    location = geolocator.geocode(city.name)
+                response = requests.get(api_url.format(location.latitude, location.longitude, weather_api_key))
+                if response.status_code == requests.codes.ok:
+                    weather = json.loads(response.text)
+                    temp = int(weather['main']['temp'])
+                    state = weather['weather'][0]['main']
 
-                utc_now = datetime.datetime.fromtimestamp(weather['dt']) # time in utc seconds
-                timezone = datetime.timezone(datetime.timedelta(seconds=weather['timezone']))
-                hour = utc_now.astimezone(timezone).hour
+                    utc_now = datetime.datetime.fromtimestamp(weather['dt']) # time in utc seconds
+                    timezone = datetime.timezone(datetime.timedelta(seconds=weather['timezone']))
+                    hour = utc_now.astimezone(timezone).hour
 
-                picture = 'day' if 19 > hour > 11 else 'evening-morning'
-                picture = 'night' if hour < 5 or hour > 22 else picture
+                    picture = 'day' if 19 > hour > 11 else 'evening-morning'
+                    picture = 'night' if hour < 5 or hour > 22 else picture
 
-                if picture == 'day' or picture == 'evening-morning':
-                    if 'rain' in state.lower():
-                        picture = 'rain'
-                    elif 'cloud' in state.lower():
-                        picture = 'clouds'
-                    elif 'snow' in state.lower():
-                        picture = 'snow'
+                    if picture == 'day' or picture == 'evening-morning':
+                        if 'rain' in state.lower():
+                            picture = 'rain'
+                        elif 'cloud' in state.lower():
+                            picture = 'clouds'
+                        elif 'snow' in state.lower():
+                            picture = 'snow'
 
-                cities.append({'city': city.name, 'temp': temp, 'state': state, 'picture': picture, 'city_id': city.id})
-                city.temperature = temp
-                city.state = state
-                city.picture = picture
-                city.timestamp = datetime.datetime.now()
-                db.session.commit()
-            else:
-                flash(f"Error: {response.status_code}, {response.text}")
+                    cities.append({'city': city.name, 'temp': temp, 'state': state, 'picture': picture, 'city_id': city.id})
+                    city.temperature = temp
+                    city.state = state
+                    city.picture = picture
+                    city.timestamp = datetime.datetime.now()
+                    db.session.commit()
+                else:
+                    flash(f"Error: {response.status_code}, {response.text}")
+            except GeocoderServiceError as e:
+                flash(f"Geocoding service error: {e}")
         else:
             cities.append({'city': city.name, 'temp': city.temperature, 'state': city.state, 'picture': city.picture, 'city_id': city.id})
     return render_template('index.html', cities=cities)
