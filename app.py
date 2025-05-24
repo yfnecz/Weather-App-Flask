8e76fe902f48e738be0fa425773fef6dff3462b8
Natalia Shishina <nshishina@odin.com>
2025-05-23 17:58:52 -0700

    add error geolocate handling

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def index():
                 else:
                     flash("The city doesn't exist!")
             except GeocoderServiceError as e:
-                flash("Some cities failed to load due to Geocoder free API request limit")
+                flash(f"Some cities failed to load due to Geocoder free API request limit {e}")
         else:
             flash("The city has already been added to the list!")
     api_url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}'
@@ -124,7 +124,7 @@ def index():
                 else:
                     flash(f"Error: {response.status_code}, {response.text}")
             except GeocoderServiceError as e:
-                flash("Some cities failed to load due to Geocoder free API request limit")
+                flash(f"Some cities failed to load due to Geocoder free API request limit {e}")
         else:
             cities.append({'city': city.name, 'temp': city.temperature, 'state': city.state, 'picture': city.picture, 'city_id': city.id})
     return render_template('index.html', cities=cities)
